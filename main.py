68c5fffce6bada39ec4fa2e1f7d3b79ce2ca24a4
Madison Simpson <madison_simpson@outlook.com>
2024-02-23 19:25:05 -0500

    Day 1/2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+import pandas
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
@@ -6,5 +8,24 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
+total_states = 50
+correct_answers = 0
 
-screen.exitonclick()
+while correct_answers < total_states:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+
+    data = pandas.read_csv("50_states.csv")
+    result = data[data["state"] == answer_state]
+
+    if not result.empty:
+        x = result["x"].values[0]
+        y = result["y"].values[0]
+        print(answer_state + ": " + str(x) + ", " + str(y))
+        t = turtle.Turtle()
+        t.penup()
+        t.goto(x, y)
+        t.hideturtle()
+        t.write(answer_state)
+        correct_answers += 1
+
+turtle.mainloop()
